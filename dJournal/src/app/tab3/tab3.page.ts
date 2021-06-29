75a18280fab4840bce3c68a4b15947c91d360e3b
Chris <christpc@csd.auth.gr>
2021-06-29 15:48:31 +0300

    committing

--- dJournal/src/app/tab3/tab3.page.ts
+++ dJournal/src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -10,3 +11,4 @@ export class Tab3Page {
   constructor() {}
 
 }
+
